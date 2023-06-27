1bba292ed7ff8a07f69d80bea8717ce852c672b7
meorl61 <meorl61@gmail.com>
2023-06-27 15:57:34 +0800

    ee

---
+++ lesson6_1.py
@@ -0,0 +1,38 @@
+import gpiozero as zero
+from time import sleep
+import RPi.GPIO as GPIO
+from datetime import datetime
+
+
+#連線遠端
+import requests 
+
+if __name__ == "__main__":
+    mcp3008_ch7 = zero.MCP3008(channel = 7)
+    mcp3008_ch6 = zero.MCP3008(channel = 6)
+    
+    try:
+        while True:
+            value_ch7 = round(mcp3008_ch7.value*100)
+            value_ch6 = round(mcp3008_ch6.value*100*3.3*2)
+            
+            if value_ch7 > 11:
+                print("光線亮")
+            else:
+                print("光線暗")
+            
+            datetimeStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            print("光敏電阻値: ", value_ch7)
+            print("LM35",value_ch6)
+            response=requests.get(f'https://meorl61-zany-enigma-6576x9469vjfr5j6-8000.preview.app.github.dev/raspberry?time={datetimeStr}&light={value_ch7}&temperature={value_ch6}')
+            if response.ok:
+                print("上傳成功")
+                print(response.text)
+            else:
+                print(response.status_code)
+            sleep(5)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("程式緊急停止")
---
+++ tempdata.py
@@ -0,0 +1,44 @@
+def sayHellow():
+    print("Hello")
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn):
+    # """ 三個" 表示多行, 
+    sql_projects = """  
+    CREATE TABLE IF NOT EXISTS tempdata (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT NOT NULL,
+		itemname TEXT NOT NULL,
+        itemvalue NUMERIC NOT NULL 
+    );
+    """
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_projects)
+        print("Table created successfully")
+    except Error as e:
+        print(e)
+
+#def insert_data(conn, state):
+def insert_data(itemname,itemvalue):    
+    conn = create_connection('iot.db')
+    create_table(conn)
+
+    sql_insert = """ INSERT INTO tempdata(date, itemname,itemvalue)
+              VALUES(datetime('now','localtime'),?,?) """
+    cur = conn.cursor()
+    cur.execute(sql_insert, (itemname,itemvalue))  #tuple寫法, 只有一個值時,最後要加逗點
+    conn.commit()
+    conn.close()
